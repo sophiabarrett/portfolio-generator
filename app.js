7fbebbd2bc0f613b18f95d06d2b37bb2bd1ae47b
Sophia Barrett <sophia@yoursummit.media>
2021-11-03 08:57:00 -0700

    init npm and install inquirer

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 // import modules
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'fullName',
+        message: 'What is your full name?'
+    }
+  ])
+  .then((answers) => {
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-    console.log('Portfolio Complete! Check out index.html to see the output!');
-});
-
-// const printProfileData = profileDataArr => {
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw new Error(err);
+//     console.log('Portfolio Complete! Check out index.html to see the output!');
+// });
