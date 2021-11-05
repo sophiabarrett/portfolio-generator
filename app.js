28b46b924ec2c90ca4a9ebc675b4bbf64f7582d9
Sophia Barrett <sophia@yoursummit.media>
2021-11-05 13:44:27 -0700

    add stylesheet

--- app.js
+++ app.js
@@ -183,10 +183,20 @@ const mockData = {
 //     .then(portfolioData => {
 //         const pageHTML = generatePage(portfolioData);
         const pageHTML = generatePage(mockData);
-        fs.writeFile('./index.html', pageHTML, err => {
-           if (err) throw new Error(err);
+        fs.writeFile('./dist/index.html', pageHTML, err => {
+            if (err) {
+                console.log(err);
+                return;
+            };
+            console.log('Page created! Check out index.html in this directory to see it.');
 
-        console.log('Page created! Check out index.html in this directory to see it.')
+            fs.copyFile('./src/style.css', './dist/style.css', err => {
+                if (err) {
+                    console.log(err);
+                    return;
+                };
+                console.log('Stylesheet copied successfully!');
+            });
         });
 //     });
 
