5ebdaac5481b98823c37cfe39b27bf1190dd9cb8
Sophia Barrett <sophia@yoursummit.media>
2021-11-06 12:48:19 -0700

    finalize app

--- app.js
+++ app.js
@@ -60,9 +60,9 @@ const promptProject = portfolioData => {
     }
     
     console.log(`
-=================
-Add a New Project
-=================
+        =================
+        Add a New Project
+        =================
     `);
     return inquirer.prompt([
         {
@@ -132,50 +132,6 @@ Add a New Project
         }
     })
 }
-
-// const mockData = {
-//     name: 'Lernantino',
-//     github: 'lernantino',
-//     about: "I'm a developer!",
-//     projects: [
-//         {
-//           name: 'Run Buddy',
-//           description:
-//             'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-//           languages: ['HTML', 'CSS'],
-//           link: 'https://github.com/lernantino/run-buddy',
-//           feature: true,
-//           confirmAddProject: true
-//         },
-//         {
-//           name: 'Taskinator',
-//           description:
-//             'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-//           languages: ['JavaScript', 'HTML', 'CSS'],
-//           link: 'https://github.com/lernantino/taskinator',
-//           feature: true,
-//           confirmAddProject: true
-//         },
-//         {
-//           name: 'Taskmaster Pro',
-//           description:
-//             'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-//           languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
-//           link: 'https://github.com/lernantino/taskmaster-pro',
-//           feature: false,
-//           confirmAddProject: true
-//         },
-//         {
-//           name: 'Robot Gladiators',
-//           description:
-//             'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
-//           languages: ['JavaScript'],
-//           link: 'https://github.com/lernantino/robot-gladiators',
-//           feature: false,
-//           confirmAddProject: false
-//         }
-//       ]
-// }
 
 promptUser()
     .then(promptProject)
